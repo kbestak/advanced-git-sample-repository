dd4845b7d5eb93603a5b774db0316a26a75ed485
Kresimir Bestak <kbestak@gmail.com>
2025-09-15 13:47:50 +0200

    sub12

--- adder/__init__.py
+++ adder/__init__.py
@@ -9,3 +9,9 @@ def subtract(x: np.int32, y: np.int32):
 
 def multiply(x: np.int32, y: np.int32):
     return x * y
+
+def sub_1(x: np.int32, y: np.int32):
+    return x - y - 1
+
+def sub_2(x: np.int32, y: np.int32):
+    return x - y - 2
